b4d86cb4882300a7a809bc00796f66d1ecf03144
Jean Teissier <jteissie@made-f0Br6s2.clusters.42paris.fr>
2024-06-18 16:10:57 +0200

    started refactoring list initialization

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jteissie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 11:54:09 by jteissie          #+#    #+#             */
-/*   Updated: 2024/06/18 14:47:44 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:09:20 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 long	ft_check_atol(char *str);
 void	handle_error(int code);
+t_stack	*create_node(long *content);
+t_stack	*add_node(t_stack *head, t_stack *tail, long *data);
+t_stack_list	*initialize(long *args, size_t size);
+long	*parse_args(char **av, size_t size);
 typedef struct s_stack 
 {
 	struct s_stack	*next;
